3876d5297e2bac68e6e5ab034fc06a404f88ecdf
Venkata Sai Raghavendra Rao Sunkavalli <sunkavalli2@usf.edu>
2021-02-01 10:08:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace trik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+            int count = 1;
+            for (int i = 0; i < x.Length; i++)
+            {
+                string l = x[i] + "";
+                if (l.Equals("A"))
+                {
+                    if (count == 1)
+                        count++;
+                    else if (count == 2)
+                        count--;
+                }
+                else if (l.Equals("B"))
+                {
+                    if (count == 2)
+                        count++;
+                    else if (count == 3)
+                        count--;
+                }
+                else
+                {
+                    if (count == 1)
+                        count += 2;
+                    else if (count == 3)
+                        count -= 2;
+                }
+            }
+            Console.WriteLine(count);
+
+        }
+    }
+}
